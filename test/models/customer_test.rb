124c23aa0089bc984cc52bc441ef086278546bd8
Your Name Here <jerryz@me.com>
2020-04-21 17:37:02 -0700

    Added initial tests for customer_name validations   Don't typically test Rails validations since   they are built in. But was messing around   trying to get deprecation warnings not to display.   To no avail I might add.
    On branch master
    Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   test/fixtures/customers.yml
    	modified:   test/models/customer_test.rb

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -4,4 +4,12 @@ class CustomerTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "insure business_name at least 5 letters" do
+	 	custs = customers(:good_cust, :bad_cust)
+	 	custs.each do |cust|
+	 		cust.save
+	 	end
+  	assert_not custs[1].save, "Saved the customer with a bad title"
+  	assert custs[0].save, "Saves valid customer name"
+  end
 end
